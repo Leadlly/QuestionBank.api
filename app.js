712d24c67eb7faa3c606d65f4451848c704f4b83
shivang-16 <shivangyadav121@gmail.com>
2024-03-31 22:25:47 +0530

    updated env and cors

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ connectedToDb();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: [process.env.FRONTEND_URL],
     credentials: true
 }));
 
--- db/db.js
+++ db/db.js
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017"
-
 const connectedToDb = async() =>{
+    const MONGO_URI = process.env.MONGO_URI
     try {
        await mongoose.connect(MONGO_URI, {
             dbName: "leadlly"
