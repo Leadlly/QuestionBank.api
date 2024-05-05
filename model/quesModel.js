366e0ab695f23d587e7358bb06868eaa6bcc0a56
ShivaniYadav07 <sagaryadavarmy813@gmail.com>
2024-05-05 17:42:56 +0530

    error handling

--- model/quesModel.js
+++ model/quesModel.js
@@ -1,47 +1,70 @@
 import mongoose, { Schema, mongo } from "mongoose";
 
-const quesSchema = new Schema({
+const quesSchema = new mongoose.Schema({
   question: {
-    type: String,
-    required: true,
+      type: String,
+      required: true,
   },
   options: {
-    all: {
-      type: Array,
-      required: true
-    },
-    correct: {
-      type: Array,
-      required: true
-    },
+      all: {
+          type: [String],
+          required: true
+      },
+      correct: {
+          type: [String],
+          required: true
+      },
   },
   standard: {
-    type: Number,
-    required: true,
+      type: Number,
+      required: true,
   },
   subject: {
-    type: String,
-    required: true,
+      type: String,
+      required: true,
   },
   chapter: {
-    type: String,
-    required: true,
+      type: String,
+      required: true,
   },
   topic: {
-    type: String,
-    required: true,
+      type: String,
+      required: true,
   },
   subtopics: {
-    type: Schema.Types.Mixed,
+      type: [{
+          name: {
+              type: String,
+              required: true,
+          },
+          subtopics: {
+              type: [
+                  {
+                      name: {
+                          type: String,
+                          required: true,
+                      },
+                      subtopics: {
+                          type: Array,
+                          default: []
+                      }
+                  }
+              ],
+              default: []
+          }
+      }],
+      default: [],
   },
   level: {
-    type: String,
-    required: true,
+      type: String,
+      required: true,
   },
   createdAt: {
-    type: Date,
-    default: Date.now,
+      type: Date,
+      default: Date.now,
   },
 });
+
+
 
 export const Ques = mongoose.model("QuestionBank", quesSchema);
