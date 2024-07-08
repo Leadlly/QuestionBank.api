2a07002e6f2ba64ab89428f58e741219cfe29b43
ShivaniYadav07 <sagaryadavarmy813@gmail.com>
2024-07-08 22:00:33 +0530

    made view cap and top

--- routes/topicRoutes.js
+++ routes/topicRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createTopic, getTopic } from "../controller/topicController.js";
+import { createTopic, getTopic, getTopicById } from "../controller/topicController.js";
 import convertToLowercase from "../middlewares/lowercase.js";
 import isAuthenticated from "../middlewares/auth.js";
 import checkAdmin from "../middlewares/checkAdmin.js";
+import { updateTopicExamTags } from "../controller/chapterController.js";
 const router = express.Router();
 
 router.post(
@@ -13,5 +14,7 @@ router.post(
     createTopic
 );
 router.get("/get/topic", getTopic);
+router.get("/get/topic/:id", getTopicById);
+router.post("/topic/:id/examtag", updateTopicExamTags);
 
 export default router;
